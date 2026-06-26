5d4eadc7b1a076b96ee2ccc90b99f21170ef3620
James Nguyen <itsreallyjamesnguyen@gmail.com>
2026-06-25 21:55:13 -0400

    created spring_boot app

---
+++ cinemabackend/src/main/java/com/csci/cinemabackend/CinemabackendApplication.java
@@ -0,0 +1,13 @@
+package com.csci.cinemabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemabackendApplication.class, args);
+	}
+
+}
