11e2648343edd967c77895c374a3f4cabbf93981
JoeB1d0me <96071431+JoeB1d0me@users.noreply.github.com>
2026-06-28 02:42:58 +0100

    Added testcase in homepage for database access

--- cinemabackend/src/main/java/com/csci/cinemabackend/CinemabackendApplication.java
+++ cinemabackend/src/main/java/com/csci/cinemabackend/CinemabackendApplication.java
@@ -1,13 +1,27 @@
 package com.csci.cinemabackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.csci.cinemabackend.repository.MovieRepository;
 
 @SpringBootApplication
 public class CinemabackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CinemabackendApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner test(MovieRepository movieRepository) {
+		return args -> {
+			System.out.println("Currently Running Movies in database:");
+
+			movieRepository.findByStatus("Currently Running")
+					.forEach(System.out::println);
+		};
 	}
 
 }
