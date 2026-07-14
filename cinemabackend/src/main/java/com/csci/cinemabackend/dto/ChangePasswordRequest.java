26d36e3b8d318c9c0a3d3b02ec63dd535dd2f8c2
jess1011 <jgs46777@uga.edu>
2026-07-14 12:00:08 -0400

    Create ChangePasswordRequest.java

---
+++ cinemabackend/src/main/java/com/csci/cinemabackend/dto/ChangePasswordRequest.java
@@ -0,0 +1,35 @@
+package com.csci.cinemabackend.dto;
+
+/**
+ * Contains the information required to change a user's password.
+ */
+public class ChangePasswordRequest {
+
+    private String currentPassword;
+    private String newPassword;
+    private String confirmPassword;
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
