37cfcefb4af35d9cadced82183e14b2887c9f926
jess1011 <jgs46777@uga.edu>
2026-07-14 11:35:28 -0400

    Create UpdateProfileRequest.java

---
+++ cinemabackend/src/main/java/com/csci/cinemabackend/dto/UpdateProfileRequest.java
@@ -0,0 +1,35 @@
+package com.csci.cinemabackend.dto;
+
+/**
+ * Contains the profile fields a user is allowed to update.
+ */
+public class UpdateProfileRequest {
+
+    private String userName;
+    private String firstName;
+    private String lastName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
