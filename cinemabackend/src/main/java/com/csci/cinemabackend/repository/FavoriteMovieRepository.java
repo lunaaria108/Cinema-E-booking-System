8c6d6d691df11f4ab2eeb51cd5447eba684dae57
jess1011 <jgs46777@uga.edu>
2026-07-18 17:06:27 -0400

    Update FavoriteMovieRepository.java

--- cinemabackend/src/main/java/com/csci/cinemabackend/repository/FavoriteMovieRepository.java
+++ cinemabackend/src/main/java/com/csci/cinemabackend/repository/FavoriteMovieRepository.java
@@ -3,10 +3,12 @@ package com.csci.cinemabackend.repository;
 import com.csci.cinemabackend.model.FavoriteMovie;
 import com.csci.cinemabackend.model.FavoriteMovieId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Provides database operations for users' favorite movies.
@@ -14,20 +16,34 @@ import java.util.List;
 public interface FavoriteMovieRepository
         extends JpaRepository<FavoriteMovie, FavoriteMovieId> {
 
-    List<FavoriteMovie> findByUserUserId(Integer userId);
+    @Query("""
+        SELECT f
+        FROM FavoriteMovie f
+        WHERE f.user.userId = :userId
+        """)
+    List<FavoriteMovie> findFavoritesByUserId(
+            @Param("userId") Integer userId
+    );
 
-   @Query("""
-SELECT COUNT(f) > 0
-FROM FavoriteMovie f
-WHERE f.user.userId = :userId
-  AND f.movie.movieId = :movieId
-""")
-boolean favoriteExists(
-        @Param("userId") Integer userId,
-        @Param("movieId") Integer movieId
-);
-    void deleteByUserUserIdAndMovieMovieId(
-            Integer userId,
-            Integer movieId
+    @Query("""
+        SELECT f
+        FROM FavoriteMovie f
+        WHERE f.user.userId = :userId
+          AND f.movie.movieId = :movieId
+        """)
+    Optional<FavoriteMovie> findFavorite(
+            @Param("userId") Integer userId,
+            @Param("movieId") Integer movieId
+    );
+
+    @Modifying
+    @Query("""
+        DELETE FROM FavoriteMovie f
+        WHERE f.user.userId = :userId
+          AND f.movie.movieId = :movieId
+        """)
+    int deleteFavorite(
+            @Param("userId") Integer userId,
+            @Param("movieId") Integer movieId
     );
 }
