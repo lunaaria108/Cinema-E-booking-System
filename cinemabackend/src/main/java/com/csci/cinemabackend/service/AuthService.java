cdd2d9551d8f6421fae5d6dff7c9d26bb5d7449e
James Nguyen <142438767+JamestheNguyenir@users.noreply.github.com>
2026-07-18 20:37:10 -0400

    Fix typo in AuthService for promoOptIn

--- cinemabackend/src/main/java/com/csci/cinemabackend/service/AuthService.java
+++ cinemabackend/src/main/java/com/csci/cinemabackend/service/AuthService.java
@@ -107,7 +107,7 @@ public class AuthService {
                                 passwordEncoder.encode(request.password()));
                 user.setIsAdmin(false);
                 user.setIsActive(false);
-                user.setPromotionOptIn(request.promoOptIn());
+                uuser.setPromoOptIn(request.promoOptIn());
 
                 user = userRepository.save(user);
 
